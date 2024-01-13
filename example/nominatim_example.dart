26bd6ddd7d1d8765d6b917e14ab6b2b58e23c0ab
Vojtěch Perník <develop@pervoj.cz>
2024-01-13 15:53:52 +0100

    Export public apis

--- example/nominatim_example.dart
+++ example/nominatim_example.dart
@@ -1,6 +1,4 @@
-import "package:nominatim/nominatim.dart";
-
 void main() {
-  var awesome = Awesome();
-  print("awesome: ${awesome.isAwesome}");
+  // var awesome = Awesome();
+  // print("awesome: ${awesome.isAwesome}");
 }
--- test/nominatim_test.dart
+++ test/nominatim_test.dart
@@ -1,16 +1,15 @@
-import "package:nominatim/nominatim.dart";
 import "package:test/test.dart";
 
-void main() {
+void main() async {
   group("A group of tests", () {
-    final awesome = Awesome();
+    // final awesome = Awesome();
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test("First Test", () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    // test("First Test", () {
+    //   expect(awesome.isAwesome, isTrue);
+    // });
   });
 }
