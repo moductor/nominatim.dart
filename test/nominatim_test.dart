18e0b93c4ff148f78f174668522010dabfc5748a
Vojtěch Perník <develop@pervoj.cz>
2024-01-13 16:14:59 +0100

    Add simple tests

--- test/nominatim_test.dart
+++ test/nominatim_test.dart
@@ -1,15 +1,19 @@
+import "package:latlong2/latlong.dart";
+import "package:nominatim/nominatim.dart";
 import "package:test/test.dart";
 
 void main() async {
-  group("A group of tests", () {
-    // final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
+  group("Methods", () {
+    test("addressToPosition", () async {
+      final position = await addressToPosition("vaclavske namesti 68 praha");
+      expect(position, equals(LatLng(50.0792052, 14.4305757)));
     });
 
-    // test("First Test", () {
-    //   expect(awesome.isAwesome, isTrue);
-    // });
+    test("addressToPosition", () async {
+      final address = await positionToAddress(LatLng(50.0792052, 14.4305757));
+      expect(address.settlementName, equals("Hlavní město Praha"));
+      expect(address.road, equals("Václavské náměstí"));
+      expect(address.houseNumber, equals("1700/68"));
+    });
   });
 }
